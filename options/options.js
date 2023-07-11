da11e2fc4cca2a06ccf76af8d0d5d9bc171baf4b
Crowdbotics <team@crowdbotics.com>
2023-07-11 07:43:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-glitter-42091",
+  url: "https://royal-glitter-42091.botics.co",
+  api: "https://royal-glitter-42091.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
